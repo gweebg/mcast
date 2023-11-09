3dfdc1e0b55607f803d900a428f5e4979e552c83
tsousa111 <tiagao2001@hotmail.com>
2023-11-09 10:06:05 +0000

    package flags and unit tests

---
+++ internal/flags/flags.go
@@ -0,0 +1,23 @@
+package flags
+
+type FlagType uint8
+
+func New() FlagType {
+	return 0
+}
+
+func (flags *FlagType) CheckFlag(f FlagType) bool {
+	return (*flags&f != 0)
+}
+
+func (flags *FlagType) SetFlag(f FlagType) {
+	*flags |= f
+}
+
+func (flags *FlagType) UnsetFlag(f FlagType) {
+	*flags &= ^f
+}
+
+func (flags *FlagType) OnlyHasFlag(f FlagType) bool {
+	return *flags == f
+}
