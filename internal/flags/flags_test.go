c80a3c69d8a955924d8b458e90fb496165d3ae78
guilherme <guilherme.g.sampaio@gmail.com>
2023-11-13 22:40:11 +0000

    fix flag tests

--- internal/flags/flags_test.go
+++ internal/flags/flags_test.go
@@ -11,29 +11,46 @@ const (
 )
 
 func TestSet(t *testing.T) {
-    var flags FlagType = 0b0
+	var flags FlagType = 0b0
 	flags.SetFlag(GET)
 	if flags != GET {
 		t.Fatalf("Expected 0b%08b, but got 0b%08b", GET, flags)
 	}
 }
 
-func TestUnset(t *testing.T){
-    var flags FlagType = 0b0
-    flags.SetFlag(SND)
-    flags.SetFlag(GET)
-    flags.UnsetFlag(SND)
-    if flags != GET {
-        t.Fatalf("Expected 0b%08b, but got 0b%08b", GET, flags)
-    } 
+func TestUnset(t *testing.T) {
+	var flags FlagType = 0b0
+
+	flags.SetFlag(SND)
+	flags.SetFlag(GET)
+	flags.UnsetFlag(SND)
+
+	if flags != GET {
+		t.Fatalf("Expected 0b%08b, but got 0b%08b", GET, flags)
+	}
 }
 
-func TestCheck(t *testing.T){
-    var flags FlagType = 0b0
-    flags.SetFlag(SND)
-    flags.SetFlag(GET)
-    flags.UnsetFlag(SND)
-    if flags.CheckFlag(SND) {
-        t.Fatalf("Expected 0b%08b, but got 0b%08b", GET, flags)
-    } 
+func TestCheck(t *testing.T) {
+	var flags FlagType = 0b0
+
+	flags.SetFlag(SND)
+	flags.SetFlag(GET)
+	flags.UnsetFlag(SND)
+
+	if flags.CheckFlag(SND) {
+		t.Fatalf("Expected 0b%08b, but got 0b%08b", GET, flags)
+	}
+}
+
+func TestHasOne(t *testing.T) {
+
+	var flags FlagType = 0b0
+
+	flags.SetFlag(GET)
+	//flags.SetFlag(ERR)
+
+	if v := flags.OnlyHasFlag(GET); !v {
+		t.Fatalf("Expected %v, but got %v", !v, v)
+	}
+
 }
